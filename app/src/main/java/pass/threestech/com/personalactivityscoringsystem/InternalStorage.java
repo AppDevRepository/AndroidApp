90f4a16bce4a2bb0aa440981fea1ab716008a51f
MobileAppRepository <gitrepo2018>
2018-05-07 22:14:19 -0500

    Internal Storage

--- app/src/main/java/pass/threestech/com/personalactivityscoringsystem/InternalStorage.java
+++ app/src/main/java/pass/threestech/com/personalactivityscoringsystem/InternalStorage.java
@@ -14,7 +14,7 @@ public final class InternalStorage{
     private InternalStorage() {}
 
     public static void writeObject(Context context, String key, Object object) throws IOException {
-        FileOutputStream fos = context.openFileOutput(key, Context.MODE_PRIVATE);
+        FileOutputStream fos = context.openFileOutput(key, context.MODE_PRIVATE);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(object);
         oos.close();
@@ -23,6 +23,7 @@ public final class InternalStorage{
 
     public static Object readObject(Context context, String key) throws IOException,
             ClassNotFoundException {
+        String path = context.getFilesDir().getAbsolutePath()+"/"+key;
         FileInputStream fis = context.openFileInput(key);
         ObjectInputStream ois = new ObjectInputStream(fis);
         Object object = ois.readObject();
